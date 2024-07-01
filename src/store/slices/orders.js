675dde901efc5d7d9c02877cef27ab8f47f73d7c
Yevhen Timonin <yevhen.timonin@corteva.com>
2024-07-01 10:41:45 +0300

    Refactoring code.

--- src/store/slices/orders.js
+++ src/store/slices/orders.js
@@ -1,65 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
+
+const sliceName = 'orders';
 
 const slice = createSlice({
-    name: 'orders',
+    name: sliceName,
     initialState: {
-        bids: [],
-        asks: [],
+        bids: {},
+        asks: {},
     },
     reducers: {
         processSnapshot(state, action) {
             const data = action.payload;
-            const snapshotBids = [];
-            const snapshotAsks = [];
 
             data.forEach(order => {
                 const [price, count, amount] = order;
                 if (amount > 0) {
-                    snapshotBids.push({ price, count, amount });
+                    state.bids[price] = { price, count, amount };
                 } else {
-                    snapshotAsks.push({ price, count, amount: -amount });
+                    state.asks[price] = { price, count, amount: -amount };
                 }
             });
-            snapshotBids.sort((item1, item2) => item1.price - item2.price);
-            snapshotAsks.sort((item1, item2) => item1.price - item2.price);
-            state.bids = snapshotBids;
-            state.asks = snapshotAsks;
         },
         processSingleMessage(state, action) {
             const [price, count, amount] = action.payload;
-            let bids = [...state.bids];
-            let asks = [...state.asks];
 
             // remove the order
             if (count === 0) {
                 if (amount === 1) {
-                    state.bids = bids.filter(order => order.price !== price);
+                    delete state.bids[price];
                 }
                 if (amount === -1) {
-                    state.asks = asks.filter(order => order.price !== price);
+                    delete state.asks[price];
                 }
-            } else if (amount > 0) {
+            } else {
                 // add or update the order
                 const order = { price, count, amount };
-                const orderIndex = bids.findIndex(order => order.price === price);
-                if (orderIndex > -1) {
-                    bids[orderIndex] = order;
+                if (amount > 0) {
+                    state.bids[price] = order;
                 } else {
-                    bids.push(order);
-                    bids.sort((item1, item2) => item1.price - item2.price);
+                    state.asks[price] = order;
                 }
-                state.bids = bids;
-            } else {
-                const order = { price, count, amount: -amount };
-                const orderIndex = asks.findIndex(order => order.price === price);
-                if (orderIndex > -1) {
-                    asks[orderIndex] = order;
-                    state.asks = asks;
-                } else {
-                    asks.push(order);
-                    asks.sort((item1, item2) => item1.price - item2.price);
-                }
-                state.asks = asks;
             }
         },
     },
@@ -69,6 +49,26 @@ export default slice;
 
 export const { processSnapshot, processSingleMessage } = slice.actions;
 
-export const selectBids = state => state.orders.bids;
+export const selectBids = () => {
+    const selectAllBids = createSelector([state => state[sliceName].bids], bids => {
+        const res = [];
+        Object.keys(bids)
+            .sort()
+            .map(bidPrice => res.push(bids[bidPrice]));
+        return res;
+    });
 
-export const selectAsks = state => state.orders.asks;
+    return selectAllBids;
+};
+
+export const selectAsks = () => {
+    const selectAllBids = createSelector([state => state[sliceName].asks], asks => {
+        const res = [];
+        Object.keys(asks)
+            .sort()
+            .map(bidPrice => res.push(asks[bidPrice]));
+        return res;
+    });
+
+    return selectAllBids;
+};
